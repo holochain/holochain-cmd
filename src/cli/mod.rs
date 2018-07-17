e06dc8dd4d5a0aa5c35d0455d309a3cbac6c2953
Julian Laubstein <contact@julianlaubstein.de>
2018-07-17 15:02:34 +0200

    Basic cap wasm compilation

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -2,12 +2,15 @@ use config::{App, Zome};
 use error::{CliError, CliResult, DefaultResult};
 use serde_json;
 use std::{
-    collections::HashMap, fs::{self, File}, path::{Path, PathBuf},
+    collections::HashMap, fs::{self, File}, io::Read, path::{Path, PathBuf},
 };
 
 const APP_CONFIG_FILE: &str = "app.json";
 const ZOMES_DIR: &str = "zomes";
 const ZOME_CONFIG_FILE: &str = "zome.json";
+
+const CAPS_DIR: &str = "caps";
+const CAP_CONFIG_FILE: &str = "cap.json";
 
 const TESTS_DIR: &str = "tests";
 const SCENARIOS_DIR: &str = "scenarios";
@@ -24,9 +27,7 @@ pub fn agent() -> CliResult<()> {
 pub fn package() -> DefaultResult<()> {
     let zomes_dir_path = PathBuf::from(ZOMES_DIR);
 
-    let zomes_dir = fs::read_dir(&zomes_dir_path)?;
-
-    let zomes_dir: Vec<_> = zomes_dir
+    let zomes_dir: Vec<_> = fs::read_dir(&zomes_dir_path)?
         .filter(|e| e.is_ok())
         .map(|e| e.unwrap().path())
         .collect();
@@ -35,33 +36,70 @@ pub fn package() -> DefaultResult<()> {
         bail!("no zomes found");
     }
 
-    let mut zome_config_files = HashMap::new();
-
-    for zome in zomes_dir {
-        if !zome.is_dir() {
-            bail!("the path {:?} is not a directory", zome);
+    for zome_path in zomes_dir {
+        if !zome_path.is_dir() {
+            bail!("the path {:?} is not a directory", zome_path);
         }
 
-        let config_file_path = zome.join(ZOME_CONFIG_FILE);
+        let config_file_path = zome_path.join(ZOME_CONFIG_FILE);
 
         if !config_file_path.exists() {
-            bail!("the path {:?} doesn't contain a zome.json file", zome);
+            bail!(
+                "the path {:?} doesn't contain a {} file",
+                zome_path,
+                ZOME_CONFIG_FILE
+            );
         }
 
         let config_file = Zome::from_file(&config_file_path)?;
 
-        let zome_wasm = compile_zome(&config_file_path, &config_file);
-
-        zome_config_files.insert(zome, zome_wasm);
+        compile_zome(&zome_path, &config_file)?;
     }
-
-    println!("{:#?}", zome_config_files);
 
     Ok(())
 }
 
-fn compile_zome<T: AsRef<Path>>(path: T, config: &Zome) -> Vec<u8> {
-    Vec::new()
+fn compile_zome<T: AsRef<Path>>(path: T, config: &Zome) -> DefaultResult<()> {
+    let caps_dir: Vec<_> = fs::read_dir(&path)?
+        .filter(|e| e.is_ok())
+        .map(|e| e.unwrap().path())
+        .collect();
+
+    for cap_path in caps_dir {
+        if !cap_path.is_dir() {
+            bail!("the path {:?} is not a directory", cap_path);
+        }
+
+        let config_file_path = cap_path.join(CAP_CONFIG_FILE);
+
+        if !config_file_path.exists() {
+            bail!(
+                "the path {:?} doesn't contain a {} file",
+                cap_path,
+                CAP_CONFIG_FILE
+            );
+        }
+
+        let compiled_wasm = compile_capabiliy(cap_path)?;
+    }
+
+    Ok(())
+}
+
+fn compile_capabiliy<T: AsRef<Path>>(path: T) -> DefaultResult<Vec<u8>> {
+    const WASM_BIN_NAME: &str = "main.wasm";
+
+    let path = PathBuf::from(path.as_ref());
+
+    let wasm_bin_path = path.join(WASM_BIN_NAME);
+
+    let mut file = File::open(wasm_bin_path)?;
+
+    let mut wasm_data = Vec::new();
+
+    file.read_to_end(&mut wasm_data)?;
+
+    Ok(wasm_data)
 }
 
 pub fn new(path: PathBuf, from: Option<String>) -> DefaultResult<()> {
