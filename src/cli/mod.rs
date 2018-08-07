67e2af4d32600b257e227d6e2df1469ef123327d
Julian Laubstein <contact@julianlaubstein.de>
2018-08-07 16:51:00 +0200

    Added falg to stip meta data

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -14,25 +14,6 @@ use std::{
     path::{Path, PathBuf},
 };
 
-pub const APP_CONFIG_FILE: &str = "app.json";
-pub const ZOMES_DIR: &str = "zomes";
-pub const ZOME_CONFIG_FILE: &str = "zome.json";
-
-pub const CAPABILITIES_DIR: &str = "capabilities";
-pub const CAPABILITY_CONFIG_FILE: &str = "capability.json";
-
-pub const ENTRY_TYPES_DIR: &str = "entry_types";
-pub const ENTRY_TYPE_VALIDATION_FILE: &str = "validation.wasm";
-pub const ENTRY_TYPE_CONFIG_FILE: &str = "type.json";
-
-pub const TESTS_DIR: &str = "tests";
-pub const SCENARIOS_DIR: &str = "scenarios";
-pub const UI_DIR: &str = "ui";
-pub const TARGET_DIR: &str = "target";
-pub const PACKAGE_ARTIFACT: &str = "out.hcpkg";
-
-pub const ZOME_WASM_BIN_NAME: &str = "main.wasm";
-
 pub fn web(port: u16) -> CliResult<()> {
     Err(CliError::UnknownLanguage)
 }
@@ -48,8 +29,8 @@ pub fn agent() -> CliResult<()> {
 
 const BUNDLE_FILE_NAME: &str = "__bundle.json";
 
-pub fn bundle() -> DefaultResult<()> {
-    let dir_obj_bundle = bundle_recurse(PathBuf::from("."))?;
+pub fn package(strip_meta: bool) -> DefaultResult<()> {
+    let dir_obj_bundle = bundle_recurse(PathBuf::from("."), strip_meta)?;
 
     let out_file = File::create(BUNDLE_FILE_NAME)?;
 
@@ -71,7 +52,7 @@ const MANIFEST_FILE_NAME: &str = "dna_manifest.json";
 
 type Object = Map<String, Value>;
 
-pub fn bundle_recurse(path: PathBuf) -> DefaultResult<Object> {
+pub fn bundle_recurse(path: PathBuf, strip_meta: bool) -> DefaultResult<Object> {
     let root: Vec<_> = path
         .read_dir()?
         .filter(|e| e.is_ok())
@@ -137,18 +118,20 @@ pub fn bundle_recurse(path: PathBuf) -> DefaultResult<Object> {
         } else if node.is_dir() {
             meta_tree.insert(file_name.into(), Value::String(DIR_ID.into()));
 
-            let sub_tree_content = bundle_recurse(node.clone())?;
+            let sub_tree_content = bundle_recurse(node.clone(), strip_meta)?;
 
             main_tree.insert(file_name.into(), Value::Object(sub_tree_content));
         }
     }
 
-    if meta_tree.len() > 0 {
-        meta_section.insert(META_TREE_SECTION_NAME.into(), Value::Object(meta_tree));
-    }
+    if !strip_meta {
+        if meta_tree.len() > 0 {
+            meta_section.insert(META_TREE_SECTION_NAME.into(), Value::Object(meta_tree));
+        }
 
-    if meta_section.len() > 0 {
-        main_tree.insert(META_SECTION_NAME.into(), Value::Object(meta_section));
+        if meta_section.len() > 0 {
+            main_tree.insert(META_SECTION_NAME.into(), Value::Object(meta_section));
+        }
     }
 
     Ok(main_tree)
@@ -173,7 +156,7 @@ fn unpack_recurse(mut obj: Object, to: PathBuf) -> DefaultResult<()> {
             for (meta_entry, meta_value) in tree_meta_obj {
                 let entry = obj
                     .remove(&meta_entry)
-                    .ok_or_else(|| format_err!("INCOMPATIBLE META SECTION"))?;
+                    .ok_or_else(|| format_err!("incompatible meta section"))?;
 
                 if let Value::String(node_type) = meta_value {
                     match node_type.as_str() {
@@ -191,10 +174,10 @@ fn unpack_recurse(mut obj: Object, to: PathBuf) -> DefaultResult<()> {
 
                             unpack_recurse(directory_obj.clone(), dir_path.clone())?;
                         }
-                        _ => bail!("YOU SUCK AT META DATA!"),
+                        _ => bail!("incompatible meta section"),
                     }
                 } else {
-                    bail!("YOU SUCK AT META DATA!");
+                    bail!("incompatible meta section");
                 }
             }
         }
@@ -216,157 +199,6 @@ fn unpack_recurse(mut obj: Object, to: PathBuf) -> DefaultResult<()> {
     Ok(())
 }
 
-/// Package smart
-pub fn package(check: bool) -> DefaultResult<()> {
-    if !check {
-        return bundle();
-    }
-
-    let zomes_dir_path = PathBuf::from(ZOMES_DIR);
-
-    let zomes_dir: Vec<_> = fs::read_dir(&zomes_dir_path)?
-        .filter(|e| e.is_ok())
-        .map(|e| e.unwrap().path())
-        .collect();
-
-    if zomes_dir.is_empty() {
-        bail!("no zomes found");
-    }
-
-    let app_config_file = File::open(APP_CONFIG_FILE)?;
-    let app_config: AppConfig = serde_json::from_reader(app_config_file)?;
-
-    let mut compiled_zomes = Vec::new();
-
-    for zome_path in zomes_dir {
-        if !zome_path.is_dir() {
-            bail!("the path {:?} is not a directory", zome_path);
-        }
-
-        let config_file_path = zome_path.join(ZOME_CONFIG_FILE);
-
-        if !config_file_path.exists() {
-            bail!(
-                "the path {:?} doesn't contain a {} file",
-                zome_path,
-                ZOME_CONFIG_FILE
-            );
-        }
-
-        let config_file = ZomeConfig::from_file(&config_file_path)?;
-
-        compiled_zomes.push(compile_zome(&zome_path, &config_file)?);
-    }
-
-    let main_dna = Dna {
-        name: app_config.name.clone(),
-
-        description: app_config.description.clone(),
-
-        version: app_config.version.to_string(),
-
-        uuid: "00000000-00000-000-00000000".into(),
-
-        dna_spec_version: "2.0.0".into(),
-
-        properties: Default::default(), // should come from the config in the future
-
-        zomes: compiled_zomes,
-    };
-
-    let mut pack = Package::from_app_config(app_config);
-
-    pack.dna = main_dna;
-
-    pack.save_as(PathBuf::from(TARGET_DIR).join(PACKAGE_ARTIFACT));
-
-    Ok(())
-}
-
-fn compile_zome<T: AsRef<Path>>(path: T, zome_config: &ZomeConfig) -> DefaultResult<Zome> {
-    let caps_dir_path = path.as_ref().join(CAPABILITIES_DIR);
-
-    let caps_dir: Vec<_> = fs::read_dir(&caps_dir_path)?
-        .filter(|e| e.is_ok())
-        .map(|e| e.unwrap().path())
-        .collect();
-
-    let mut caps = Vec::new();
-
-    for cap_path in caps_dir {
-        if !cap_path.is_dir() {
-            bail!("the path {:?} is not a directory", cap_path);
-        }
-
-        let config_file_path = cap_path.join(CAPABILITY_CONFIG_FILE);
-
-        if !config_file_path.exists() {
-            bail!(
-                "the path {:?} doesn't contain a {} file",
-                cap_path,
-                CAPABILITY_CONFIG_FILE
-            );
-        }
-
-        let cap_config_file = CapabilityConfig::from_file(config_file_path)?;
-
-        let compiled_cap = compile_capabiliy(cap_path, &cap_config_file)?;
-
-        caps.push(compiled_cap);
-    }
-
-    Ok(Zome {
-        name: zome_config.name.clone(),
-        description: zome_config.description.clone().unwrap_or_default(),
-        config: zome_config.config.clone(),
-        entry_types: Vec::new(),
-        capabilities: caps,
-    })
-
-    // let entry_types_dir_path = path.as_ref().join(ENTRY_TYPES_DIR);
-    //
-    // let entry_types_dir: Vec<_> = fs::read_dir(&entry_types_dir_path)?
-    //     .filter(|e| e.is_ok())
-    //     .map(|e| e.unwrap().path())
-    //     .collect();
-    //
-    // for entry_path in entry_types_dir {
-    //     if !entry_path.is_dir() {
-    //         bail!("{:?} is not a directory", entry_path);
-    //     }
-    //
-    //     let mut config_file = EntryTypeConfig::from_file(entry_path.join(ENTRY_TYPE_CONFIG_FILE))?;
-    //
-    //     let mut validation_file = File::open(entry_path.join(ENTRY_TYPE_VALIDATION_FILE))?;
-    //
-    //     let mut wasm_data = Vec::new();
-    //
-    //     validation_file.read_to_end(&mut wasm_data)?;
-    // }
-}
-
-fn compile_capabiliy<T: AsRef<Path>>(
-    path: T,
-    cap_config: &CapabilityConfig,
-) -> DefaultResult<Capability> {
-    let path = PathBuf::from(path.as_ref());
-
-    let wasm_bin_path = path.join(ZOME_WASM_BIN_NAME);
-
-    let mut file = File::open(wasm_bin_path)?;
-
-    let mut wasm_data = Vec::new();
-
-    file.read_to_end(&mut wasm_data)?;
-
-    Ok(Capability {
-        name: Default::default(),
-        capability: Default::default(),
-        fn_declarations: Vec::new(),
-        code: DnaWasm { code: wasm_data },
-    })
-}
-
 pub fn new(path: PathBuf, from: Option<String>) -> DefaultResult<()> {
     if !path.exists() {
         fs::create_dir_all(&path)?;
@@ -378,12 +210,12 @@ pub fn new(path: PathBuf, from: Option<String>) -> DefaultResult<()> {
         }
     }
 
-    fs::create_dir_all(path.join(ZOMES_DIR))?;
-    fs::create_dir_all(path.join(TESTS_DIR))?;
-    fs::create_dir_all(path.join(SCENARIOS_DIR))?;
-    fs::create_dir_all(path.join(UI_DIR))?;
+    fs::create_dir_all(path.join("zomes"))?;
+    fs::create_dir_all(path.join("tests"))?;
+    fs::create_dir_all(path.join("scenarios"))?;
+    fs::create_dir_all(path.join("ui"))?;
 
-    let app_config_file = File::create(path.join(APP_CONFIG_FILE))?;
+    let app_config_file = File::create(path.join("app.json"))?;
     serde_json::to_writer_pretty(app_config_file, &AppConfig::default())?;
 
     println!("Created new Holochain project at: {:?}", path);
--- src/main.rs
+++ src/main.rs
@@ -27,23 +27,31 @@ use structopt::StructOpt;
 #[structopt(about = "A command line for Holochain")]
 enum Cli {
     #[structopt(
-        name = "web", alias = "w", about = "Starts a web server for the current Holochain app"
+        name = "web",
+        alias = "w",
+        about = "Starts a web server for the current Holochain app"
     )]
     Web {
         #[structopt(long = "port", short = "p", default_value = "3000")]
         port: u16,
     },
-    #[structopt(name = "agent", alias = "a", about = "Starts a Holochain node as an agent")]
+    #[structopt(
+        name = "agent",
+        alias = "a",
+        about = "Starts a Holochain node as an agent"
+    )]
     Agent,
     #[structopt(
-        name = "package", alias = "p", about = "Builds the current Holochain app into a .hcpkg file"
+        name = "package",
+        alias = "p",
+        about = "Builds the current Holochain app into a .hcpkg file"
     )]
     Package {
         #[structopt(
-            long = "check",
-            help = "Enables strong checking of directory struxcture and configuration format"
+            long = "strip-meta",
+            help = "Strips all __META__ sections off the target bundle. Makes unpacking of the bundle impossible"
         )]
-        check: bool,
+        strip_meta: bool,
     },
     #[structopt(name = "unpack")]
     Unpack {
@@ -53,7 +61,9 @@ enum Cli {
         to: PathBuf,
     },
     #[structopt(
-        name = "init", alias = "i", about = "Initializes a new Holochain app at the given directory"
+        name = "init",
+        alias = "i",
+        about = "Initializes a new Holochain app at the given directory"
     )]
     Init {
         #[structopt(parse(from_os_str))]
@@ -94,8 +104,8 @@ fn run() -> HolochainResult<()> {
     match args {
         Cli::Web { port } => cli::web(port).or_else(|err| Err(HolochainError::Cli(err)))?,
         Cli::Agent => cli::agent().or_else(|err| Err(HolochainError::Cli(err)))?,
-        Cli::Package { check } => {
-            cli::package(check).or_else(|err| Err(HolochainError::Default(err)))?
+        Cli::Package { strip_meta } => {
+            cli::package(strip_meta).or_else(|err| Err(HolochainError::Default(err)))?
         }
         Cli::Unpack { path, to } => {
             cli::unpack(path, to).or_else(|err| Err(HolochainError::Default(err)))?
