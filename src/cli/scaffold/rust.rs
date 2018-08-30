edb437e8e7262321b04baac2d1c08a104923e04a
Connor Turland <connorturland@gmail.com>
2018-08-30 15:55:59 -0400

    change scaffold to lib and add config for it

--- src/cli/scaffold/rust.rs
+++ src/cli/scaffold/rust.rs
@@ -31,17 +31,17 @@ impl Scaffold for RustScaffold {
     fn gen<P: AsRef<Path>>(&self, base_path: P) -> DefaultResult<()> {
         fs::create_dir_all(&base_path)?;
 
-        // use cargo to initialise a Rust app without any version control
+        // use cargo to initialise a library Rust crate without any version control
         util::run_cmd(
             base_path.as_ref().to_path_buf(),
             "cargo".into(),
-            vec!["init".to_owned(), "--vcs".to_owned(), "none".to_owned()],
+            vec!["init".to_owned(), "--lib".to_owned(), "--vcs".to_owned(), "none".to_owned()],
         )?;
 
         // add hdk-rust dependency by default
         let cargo_file_path = base_path.as_ref().join(package::CARGO_FILE_NAME);
 
-        let mut f = OpenOptions::new()
+        let mut cargo_file = OpenOptions::new()
             .append(true)
             .open(cargo_file_path)?;
 
@@ -51,14 +51,19 @@ impl Scaffold for RustScaffold {
         // the above TODO hasn't been addressed, this should also be updated
         let hdk_dep: &str = "hdk = { git = \"https://github.com/holochain/hdk-rust\", branch = \"master\" }";
 
-        f.write_all(hdk_dep.as_bytes())?;
+        cargo_file.write_all(hdk_dep.as_bytes())?;
+
+        // add WASM friendly lib configuration properties
+        let lib_config: &str = "\n\n[lib]\npath = \"src/lib.rs\"\ncrate-type = [\"cdylib\"]";
+
+        cargo_file.write_all(lib_config.as_bytes())?;
 
         // create and fill in a build file appropriate for Rust
         let build_file_path = base_path.as_ref().join(package::BUILD_CONFIG_FILE_NAME);
 
-        let file = File::create(build_file_path)?;
+        let build_file = File::create(build_file_path)?;
 
-        serde_json::to_writer_pretty(file, &self.build_template)?;
+        serde_json::to_writer_pretty(build_file, &self.build_template)?;
 
         Ok(())
     }
