db3bcbc914202a78b69a4d8c8e717b66dd6f242f
Connor Turland <connorturland@gmail.com>
2018-10-03 14:48:45 -0400

    clone test files from js-tests-scaffold

--- src/cli/new.rs
+++ src/cli/new.rs
@@ -4,13 +4,42 @@ use cli::package::{
     IGNORE_FILE_NAME,
     DEFAULT_BUNDLE_FILE_NAME
 };
+use util;
 use error::DefaultResult;
 use serde_json;
 use std::{
     fs::{self, File},
-    path::PathBuf,
+    path::{Path, PathBuf},
     io::Write,
 };
+
+fn setup_test_folder(path: &PathBuf) -> DefaultResult<()> {
+    let test_dir = path.join("test");
+    fs::create_dir_all(&test_dir)?;
+
+    // clone the default test files in, without git history
+    let args: Vec<String> = [
+        "clone".to_string(),
+        "--depth".to_string(),
+        "1".to_string(),
+        "https://github.com/holochain/js-tests-scaffold.git".to_string()
+    ].to_vec();
+    util::run_cmd(path.clone(), "git".to_string(), args)?;
+
+    // copy each file from 'js-tests-scaffold/files' into 'test' folder
+    let temp_dir = path.join("js-tests-scaffold");
+    for entry in fs::read_dir(temp_dir.join("files").as_path())? {
+        let entry = entry?;
+        let from_file = entry.path();
+        let to_file = test_dir.join(Path::new(&entry.file_name()));
+        fs::copy(from_file, to_file.as_path())?;
+    }
+
+    // remove the cloned folder
+    fs::remove_dir_all(temp_dir)?;
+
+    Ok(())
+}
 
 pub fn new(path: &PathBuf, _from: &Option<String>) -> DefaultResult<()> {
     if !path.exists() {
@@ -23,6 +52,7 @@ pub fn new(path: &PathBuf, _from: &Option<String>) -> DefaultResult<()> {
         }
     }
 
+    // create empty zomes folder
     fs::create_dir_all(path.join("zomes"))?;
 
     let app_config_file = File::create(path.join("app.json"))?;
@@ -32,6 +62,8 @@ pub fn new(path: &PathBuf, _from: &Option<String>) -> DefaultResult<()> {
     let ignores = format!("{}\n{}\n", "README.md", &DEFAULT_BUNDLE_FILE_NAME);
     let mut hcignore_file = File::create(path.join(&IGNORE_FILE_NAME))?;
     hcignore_file.write_all(ignores.as_bytes())?;
+
+    setup_test_folder(&path)?;
 
     println!(
         "{} new Holochain project at: {:?}",
