8433654289e1feed544acc644961d0792645e06e
Michael Dougherty <maackle.d@gmail.com>
2018-10-18 18:21:22 -0700

    Use reasonable name for Cargo.toml; PR feedback

--- src/cli/scaffold/rust.rs
+++ src/cli/scaffold/rust.rs
@@ -9,8 +9,8 @@ use std::{
     io::SeekFrom,
     path::Path,
 };
-use toml;
 use toml::{
+    self,
     value::{
         Value
     }
@@ -24,39 +24,39 @@ pub struct RustScaffold {
 }
 
 /// Modify Cargo.toml in place, using pieces of the original
-fn rewrite_cargo_toml(cargo_file_path: &Path) -> DefaultResult<()> {
+fn rewrite_cargo_toml(base_path: &Path) -> DefaultResult<()> {
+    let cargo_file_path = base_path.join(CARGO_FILE_NAME);
     let mut cargo_file = OpenOptions::new()
         .read(true).write(true).open(cargo_file_path)?;
     let mut contents = String::new();
     cargo_file.read_to_string(&mut contents)?;
 
     // create new Cargo.toml using pieces of the original
-    let new_toml = flesh_out_cargo_toml(contents.as_str())?;
+    let zome_name = get_zome_name(base_path);
+    let new_toml = generate_cargo_toml(zome_name.as_str(), contents.as_str())?;
     cargo_file.seek(SeekFrom::Start(0))?;
     cargo_file.write_all(new_toml.as_bytes())?;
     Ok(())
 }
 
-/// Use the Cargo.toml.template file and interpolate values into the placeholders
-fn interpolate_cargo_template(authors: &Value, edition: &Value) -> DefaultResult<String> {
-    let authors = toml::to_string(authors)?;
-    let edition = toml::to_string(edition)?;
-    let template = include_str!("rust/Cargo.toml.template");
-    Ok(
-        template
-        .replace("<<AUTHORS>>", authors.as_str())
-        .replace("<<EDITION>>", edition.as_str())
-    )
+/// Assuming the base_path is at `[zomename]/code`, get the zome name
+fn get_zome_name(base_path: &Path) -> String {
+    base_path.parent()
+        .and_then(|p| p.to_str())
+        .unwrap_or("myzome")
+        .replace("/", "-")
 }
 
 /// Given existing Cargo.toml string, pull out some values and return a new
 /// string with values pulled from template
-fn flesh_out_cargo_toml(contents: &str) -> DefaultResult<String> {
+fn generate_cargo_toml(name: &str, contents: &str) -> DefaultResult<String> {
     let config: Value = toml::from_str(contents)?;
 
     let authors_default = Value::from("[\"TODO\"]");
     let edition_default = Value::from("\"TODO\"");
     let maybe_package = config.get("package");
+
+    let name = Value::from(name);
     let authors = maybe_package
         .and_then(|p| p.get("authors"))
         .unwrap_or(&authors_default);
@@ -64,7 +64,18 @@ fn flesh_out_cargo_toml(contents: &str) -> DefaultResult<String> {
         .and_then(|p| p.get("edition"))
         .unwrap_or(&edition_default);
 
-    interpolate_cargo_template(authors, edition)
+    interpolate_cargo_template(&name, authors, edition)
+}
+
+/// Use the Cargo.toml.template file and interpolate values into the placeholders
+fn interpolate_cargo_template(name: &Value, authors: &Value, edition: &Value) -> DefaultResult<String> {
+    let template = include_str!("rust/Cargo.template.toml");
+    Ok(
+        template
+        .replace("<<NAME>>", toml::to_string(name)?.as_str())
+        .replace("<<AUTHORS>>", toml::to_string(authors)?.as_str())
+        .replace("<<EDITION>>", toml::to_string(edition)?.as_str())
+    )
 }
 
 impl RustScaffold {
@@ -97,8 +108,7 @@ impl Scaffold for RustScaffold {
 
         // immediately rewrite the generated Cargo file, using some values
         // and throwing away the rest
-        let cargo_file_path = base_path.as_ref().join(CARGO_FILE_NAME);
-        rewrite_cargo_toml(&cargo_file_path)?;
+        rewrite_cargo_toml(base_path.as_ref())?;
 
         // create and fill in a build file appropriate for Rust
         let build_file_path = base_path.as_ref().join(package::BUILD_CONFIG_FILE_NAME);
